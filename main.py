ca698b5be3877978ba9d792f7af7d1621ffb0ad7
stepashka <cociorvastefan@gmail.com>
2024-07-28 06:21:51 +0300

    add fucns on main.py

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
 from fastapi import FastAPI
-
+import uvicorn
+from pydantic import EmailStr, BaseModel
 app = FastAPI()
+
+class CreateUser(BaseModel):
+    email: EmailStr
 
 @app.get('/')
 def hello_index():
     return {
         "message": "Hello index,"
     }
+
+@app.get('/items/')
+def list_items():
+    return [
+        'Item1',
+        'Item2',
+        'Item3',
+    ]
+
+@app.get('/items/{item_id}/')
+def get_item_by_id(item_id: int):
+    return {
+        'item': {
+            'id': item_id,
+        }
+    }
+
+@app.post('/user/')
+def create_user(user: CreateUser):
+    return {
+        'message': 'succes',
+        'email': user.email,
+    }
+
+
+@app.get('/hello/')
+def hello(name: str = 'World'):
+    name = name.strip().title()
+    return {'message': f'Hello {name}'}
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', reload=True)
